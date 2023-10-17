2f5627d427c1b91bf76145453cac0096c678ac5d
al3x-13 <alexandreromba@hotmail.com>
2023-10-17 23:40:35 +0100

    small ui tweaks

--- frontend/src/lib/assets/LogoWithText.svelte
+++ frontend/src/lib/assets/LogoWithText.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex flex-col items-center justify-center">
     <Logo size={230} />
-    <h1 class={`text-[2.5rem] text-color-primary-light dark:text-color-primary-dark font-oxygen font-[700] mt-10`}>AWS Management Portal</h1>
+    <h1 class={`text-[2.5rem] text-color-primary-light dark:text-color-primary-dark font-oxygen font-[700] mt-10 select-none`}>AWS Management Portal</h1>
 </div>
