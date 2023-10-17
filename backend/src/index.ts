efe1a89071c9ca18f24ec3578dc4eec808cd509e
al3x-13 <alexandreromba@hotmail.com>
2023-10-18 00:55:39 +0100

    adds prototype endpoints to create and terminate ec2 instances

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,64 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { fromEnv } from '@aws-sdk/credential-providers';
+import { EC2Client, RunInstancesCommand, RunInstancesCommandInput, TerminateInstancesCommand, TerminateInstancesCommandInput } from '@aws-sdk/client-ec2';
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
+// TODO: remove this
+const client = new EC2Client({
+	credentials: fromEnv(),
+	region: 'eu-west-2'
+});
+let instanceIds: string[] = [];
+
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+	console.log('LOL', process.env.AWS_ACCESS_KEY_ID);
 });
 
-app.get('/test', (req, res) => {
-	res.send('Hello World!');
+app.get('/create', async (req, res) => {
+	const instanceInput: RunInstancesCommandInput = {
+		ImageId: 'ami-04fb7beeed4da358b',	// Amazon Linux 2023 AMI
+		InstanceType: 't2.micro',
+		MinCount: 1,
+		MaxCount: 1
+	};
+
+	const command = new RunInstancesCommand(instanceInput);
+	try {
+		const response = await client.send(command);
+		const instance = response.Instances?.at(0)?.InstanceId;
+		if (instance) {
+			instanceIds.push(instance);
+		}
+
+		console.log(response);
+		res.send(`Instance ${instanceIds} created successfully.`);
+	} catch (err) {
+		console.log(err);
+		res.send(`Error ocurred while creating instace: ${instanceIds}.`);
+	}
+});
+
+app.get('/terminate', async (req, res) => {
+	console.log("InstanceIds:", instanceIds);
+	const terminateInput: TerminateInstancesCommandInput = {
+		InstanceIds: instanceIds
+	};
+	
+	const command = new TerminateInstancesCommand(terminateInput);
+	try {
+		const response = await client.send(command);
+		console.log(response);
+		res.send(`Instance ${instanceIds} successfully deleted.`);
+	} catch (err) {
+		console.log(err);
+		res.send(`Error ocurred while deleting instance: ${instanceIds}.`)
+	}
 });
 
 app.listen(port, () => {
