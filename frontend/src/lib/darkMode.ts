d0fe5ae92ae4d4c8a0d22dc6a20ba596c68f2390
al3x-13 <alexandreromba@hotmail.com>
2023-10-17 17:12:18 +0100

    implements dark mode

--- frontend/src/lib/darkMode.ts
+++ frontend/src/lib/darkMode.ts
@@ -1,7 +1,45 @@
+const LOCAL_STORAGE_KEY = 'light-mode';
+
+/**
+ * Gets the light mode saved preference.
+ * @returns 'dark', 'light' or null
+ */
+function getLightModePreference(): string | null {
+    let preference = window.localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (preference == null || preference == 'light' || preference == 'dark') {
+        return preference
+    }
+    return null;
+}
+
+/**
+ * Sets light mode ('light' or 'dark') preference in local storage.
+ * @param mode desired mode
+ */
+function setLightModePreference(mode: string) {
+    window.localStorage.setItem(LOCAL_STORAGE_KEY, mode);
+}
+
+/**
+ * Switches light mode on.
+ */
+export function setLightMode() {
+    setLightModePreference('light');
+    document.documentElement.classList.remove('dark');
+}
+
+/**
+ * Switches dark mode on.
+ */
+export function setDarkMode() {
+    setLightModePreference('dark');
+    document.documentElement.classList.add('dark');
+}
+
+/**
+ * Gets dark mode state.
+ * @returns Whether dark mode is active
+ */
 export function darkModeActive(): boolean {
     return document.documentElement.classList.contains('dark');
 }
-
-// TODO
-// - Dark mode toggle functionality
-// - Save preference (localstorage)
