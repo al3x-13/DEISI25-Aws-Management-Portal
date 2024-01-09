7647ca825ece1581fdee347064ef51aa0ca28c28
al3x-13 <alexandreromba@hotmail.com>
2024-01-09 01:10:25 +0000

    chore: update dev db

--- db/dev-db.sql
+++ db/dev-db.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.1 (Ubuntu 16.1-1.pgdg22.04+1)
+-- Dumped from database version 16.1
 -- Dumped by pg_dump version 16.1 (Ubuntu 16.1-1.pgdg22.04+1)
 
 SET statement_timeout = 0;
@@ -19,42 +19,6 @@ SET row_security = off;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
-
---
--- Name: sessions; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE public.sessions (
-    id integer NOT NULL,
-    user_id integer,
-    session_token text NOT NULL,
-    expires timestamp without time zone NOT NULL
-);
-
-
-ALTER TABLE public.sessions OWNER TO admin;
-
---
--- Name: sessions_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
---
-
-CREATE SEQUENCE public.sessions_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE public.sessions_id_seq OWNER TO admin;
-
---
--- Name: sessions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
---
-
-ALTER SEQUENCE public.sessions_id_seq OWNED BY public.sessions.id;
-
 
 --
 -- Name: users; Type: TABLE; Schema: public; Owner: admin
@@ -95,13 +59,6 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
--- Name: sessions id; Type: DEFAULT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.sessions ALTER COLUMN id SET DEFAULT nextval('public.sessions_id_seq'::regclass);
-
-
---
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: admin
 --
 
@@ -109,49 +66,20 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
--- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: admin
---
-
-COPY public.sessions (id, user_id, session_token, expires) FROM stdin;
-\.
-
-
---
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
 COPY public.users (id, username, password_hash, email, admin, created_at) FROM stdin;
+1	test	$2a$10$F14lHBQeLpTWkR/.UtKcZuMJP4wIoDqOGpGqjyLVLDSt0vpOPPA86	test@example.com	t	2024-01-08 17:45:19.091531
+2	john	$2a$10$ioCMXlqSUHkhpDgni8VdPO1tXAc2hBrhYrh/QseecFANLgjUin3sK	john@deisi25.com	f	2024-01-09 00:04:03.357039
 \.
-
-
---
--- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
---
-
-SELECT pg_catalog.setval('public.sessions_id_seq', 1, false);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 1, false);
-
-
---
--- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_pkey PRIMARY KEY (id);
-
-
---
--- Name: sessions sessions_user_id_key; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_user_id_key UNIQUE (user_id);
+SELECT pg_catalog.setval('public.users_id_seq', 2, true);
 
 
 --
@@ -176,14 +104,6 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
---
--- Name: sessions sessions_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --
