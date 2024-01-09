235ba4ada03d1db0f6affef87de4439a3d4f6c40
al3x-13 <alexandreromba@hotmail.com>
2024-01-09 07:01:40 +0000

    db: add roles

--- db/dev-db.sql
+++ db/dev-db.sql
@@ -21,15 +21,49 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: roles; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.roles (
+    id integer NOT NULL,
+    role character varying(30) NOT NULL
+);
+
+
+ALTER TABLE public.roles OWNER TO admin;
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE; Schema: public; Owner: admin
+--
+
+CREATE SEQUENCE public.roles_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.roles_id_seq OWNER TO admin;
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: admin
+--
+
+ALTER SEQUENCE public.roles_id_seq OWNED BY public.roles.id;
+
+
+--
 -- Name: users; Type: TABLE; Schema: public; Owner: admin
 --
 
 CREATE TABLE public.users (
     id integer NOT NULL,
-    username character varying(50) NOT NULL,
+    username character varying(100) NOT NULL,
     password_hash character varying(255) NOT NULL,
-    email character varying(255),
-    admin boolean DEFAULT false NOT NULL,
+    email character varying(255) NOT NULL,
+    role integer NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
@@ -59,6 +93,13 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: roles id; Type: DEFAULT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.roles ALTER COLUMN id SET DEFAULT nextval('public.roles_id_seq'::regclass);
+
+
+--
 -- Name: users id; Type: DEFAULT; Schema: public; Owner: admin
 --
 
@@ -66,20 +107,47 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: admin
+--
+
+COPY public.roles (id, role) FROM stdin;
+1	root
+2	admin
+3	user
+\.
+
+
+--
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY public.users (id, username, password_hash, email, admin, created_at) FROM stdin;
-1	test	$2a$10$F14lHBQeLpTWkR/.UtKcZuMJP4wIoDqOGpGqjyLVLDSt0vpOPPA86	test@example.com	t	2024-01-08 17:45:19.091531
-2	john	$2a$10$ioCMXlqSUHkhpDgni8VdPO1tXAc2hBrhYrh/QseecFANLgjUin3sK	john@deisi25.com	f	2024-01-09 00:04:03.357039
+COPY public.users (id, username, password_hash, email, role, created_at) FROM stdin;
+1	root	$2a$10$DBEM4vsmKCmT9JC1TsZMe.VNDg.iTsEL1NP7Z9xyO6xKSalnwFpxW	root@deisi25.tfc	1	2024-01-09 06:58:56.152767
+2	test	$2a$10$F14lHBQeLpTWkR/.UtKcZuMJP4wIoDqOGpGqjyLVLDSt0vpOPPA86	test@deisi25.tfc	2	2024-01-09 06:59:25.233209
+3	john	$2a$10$ioCMXlqSUHkhpDgni8VdPO1tXAc2hBrhYrh/QseecFANLgjUin3sK	john@deisi25.tfc	3	2024-01-09 06:59:52.815116
 \.
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
+--
+
+SELECT pg_catalog.setval('public.roles_id_seq', 3, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: admin
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_id_seq', 3, true);
+
+
+--
+-- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.roles
+    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
 
 
 --
@@ -104,6 +172,14 @@ ALTER TABLE ONLY public.users
 
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- Name: users users_role_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_role_fkey FOREIGN KEY (role) REFERENCES public.roles(id);
 
 
 --
