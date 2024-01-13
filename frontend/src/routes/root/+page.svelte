9cae2b73dac2926a2074c878ae4c87586f7df136
al3x-13 <alexandreromba@hotmail.com>
2024-01-13 19:03:43 +0000

    feat: root account base layout

---
+++ frontend/src/routes/root/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<div class="">
+	<div>TESTING</div>
+	<div>TESTING</div>
+	<div>TESTING</div>
+	<div>TESTING</div>
+	<div>TESTING</div>
+	<div>TESTING</div>
+</div>
