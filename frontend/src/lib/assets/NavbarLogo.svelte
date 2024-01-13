13a04319602254845a96ddb147b0382df233ce46
al3x-13 <alexandreromba@hotmail.com>
2024-01-13 05:05:51 +0000

    ui: navbar logo component

---
+++ frontend/src/lib/assets/NavbarLogo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Logo from './Logo.svelte';
+</script>
+
+<div class="flex items-center justify-center space-x-3">
+	<Logo size={42} />
+	<span class="text-base font-[600] text-color-primary-light dark:text-color-primary-dark"
+		>AWS Management Portal</span
+	>
+</div>
