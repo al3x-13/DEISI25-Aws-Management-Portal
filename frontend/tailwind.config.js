0920f96d853f3cc6c1dcf07acba8c05dd180907c
al3x-13 <alexandreromba@hotmail.com>
2024-01-13 05:08:53 +0000

    chore: ui customization

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,12 +5,24 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        oxygen: ['Oxygen', 'sans-serif']
+		lato: ['Lato', 'sans-serif'],
+        oxygen: ['Oxygen', 'sans-serif'],
       },
       colors: {
+		'bg-dark': '#0F1217',
+		'bg-light': '#ECECEC',
+		'bg2-dark': '#19212E',
+		'bg2-light': '#DFDFDF',
         'color-primary-dark': '#0B79DE',
-        'color-primary-light': '#3D97EA'
-      }
+        'color-primary-light': '#3D97EA',
+		'border-dark': '#283245',
+		'border-light': '#CFCFCF',
+		'hover-dark': '#26334B',
+		'hover-light': '',
+      },
+	  borderRadius: {
+		  'custom': '6px',
+	  }
     },
   },
   plugins: [],
