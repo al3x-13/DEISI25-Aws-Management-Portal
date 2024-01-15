0dd85639030bba4eae800a8b83027f10e3d0077c
al3x-13 <alexandreromba@hotmail.com>
2024-01-15 18:31:14 +0000

    refactor: remove irrelevant 'head' tag

--- frontend/src/routes/root/+layout.svelte
+++ frontend/src/routes/root/+layout.svelte
@@ -6,17 +6,6 @@
 	export let data: LayoutData;
 </script>
 
-<head>
-	<meta charset="UTF-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap"
-		rel="stylesheet"
-	/>
-</head>
-
 <div class="bg-bg-light dark:bg-bg-dark min-h-screen">
 	<Header username={data.props.user.username} role={data.props.user.role} />
 	<div class="w-full h-[60px]" />
