df7467e085d70281b05e61c4540503ac20618041
al3x-13 <alexandreromba@hotmail.com>
2024-01-17 05:22:16 +0000

    ui: update sidebar item

--- frontend/src/lib/widgets/SidebarItem.svelte
+++ frontend/src/lib/widgets/SidebarItem.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
 	export let ionIcon: string;
 	export let label: string;
+	export let href: string;
 	export let open: boolean;
 </script>
 
-<button type="button" class="flex items-center justify-center px-[4px]">
+<a {href} type="button" class="flex items-center justify-start ml-1 p-1 group cursor-pointer">
 	<ion-icon
 		name={ionIcon}
-		class="text-[31px] text-color-primary-light dark:text-color-primary-dark p-1 rounded-custom hover:bg-hover-light dark:hover:bg-hover-dark"
+		class="text-[31px] text-color-primary-light dark:text-color-primary-dark group-hover:text-color-hover-light dark:group-hover:text-color-hover-dark"
 	/>
 
 	{#if open}
-		<p class="ml-6">{label}</p>
+		<p
+			class="ml-6 text-xl font-semibold text-color-primary-light dark:text-color-primary-dark group-hover:text-color-hover-light dark:group-hover:text-color-hover-dark"
+		>
+			{label}
+		</p>
 	{/if}
-</button>
+</a>
