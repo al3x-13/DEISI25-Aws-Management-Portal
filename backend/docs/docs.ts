d4317311ea6fe30b1eef01d49dc695a31eb2d389
al3x-13 <alexandreromba@hotmail.com>
2024-02-02 21:32:10 +0000

    docs: build docs funcionality

--- backend/docs/docs.ts
+++ backend/docs/docs.ts
@@ -1,14 +1,8 @@
 import { OpenAPIObject, OperationObject, SecuritySchemeObject } from "openapi3-ts/oas31";
-import { addBadRequestResponseToPathMethods } from "./docs-helpers";
+import { addAuthenticationErrorResponseToPathMethods, addBadRequestResponseToPathMethods, addSecuritySchemeToPathOptions, addServerErrorResponseToPathMethods } from "./docs-helpers";
+import { apiErrorSchema, securityConfig } from "./object-schemas";
 
 function buildDocsFromTSRestOAS(tsRestOAS: OpenAPIObject): OpenAPIObject {
-	const securityConfig: SecuritySchemeObject = {
-		type: 'apiKey',
-		name: 'token',
-		in: 'cookie',
-		description: 'Json Web Token (JWT) that identifies the subject'
-	};
-
 	const docsOAS: OpenAPIObject = {
 		openapi: '3.0.1',
 		info: {
@@ -19,6 +13,9 @@ function buildDocsFromTSRestOAS(tsRestOAS: OpenAPIObject): OpenAPIObject {
 		paths: tsRestOAS.paths,
 		tags: tsRestOAS.tags,
 		components: {
+			schemas: {
+				ApiError: apiErrorSchema,
+			},
 			parameters: {
 				ContentTypeHeader: {
 					name: 'Content-Type',
@@ -36,62 +33,26 @@ function buildDocsFromTSRestOAS(tsRestOAS: OpenAPIObject): OpenAPIObject {
 		},
 	};
 
+	if (docsOAS.paths != undefined) {
+		const unprotectedPaths = [
+			'/',
+			'/test',
+			'/auth/authenticate'
+		];
 
-	tsRestOAS.components = {
-		parameters: {
-			ContentTypeHeader: {
-				name: 'Content-Type',
-				in: 'header',
-				required: true,
-				schema: {
-					type: 'string'
-				},
-				description: "Header required in all requests. It must bet set to 'application/json'."
+		for (let [path, data] of Object.entries(docsOAS.paths)) {
+			data = addBadRequestResponseToPathMethods(data);
+			data = addServerErrorResponseToPathMethods(data);
+
+			if (!unprotectedPaths.includes(path)) {
+				data = addAuthenticationErrorResponseToPathMethods(data);
+				data = addSecuritySchemeToPathOptions(data);
+
+				// DEBUG
+				// console.log(`Route: ${path}, Data: ${JSON.stringify(data, null, 2)}`);
 			}
-		},
-		headers: {
-			ContentTypeHeader: {
-				required: true,
-				schema: {
-					type: 'string'
-				},
-				description: "Header required in all requests. It must bet set to 'application/json'."
-			}
-		},
-		securitySchemes: {
-			cookieAuth: securityConfig,
-		},
-		...tsRestOAS.components
-	};
-
-
-	// if (docsOAS.paths != undefined) {
-	// 	for (let [path, data] of Object.entries(docsOAS.paths)) {
-	// 		const unprotectedPaths = [
-	// 			'/',
-	// 			'/test',
-	// 			'/auth/authenticate'
-	// 		];
-	//
-	// 		if (unprotectedPaths.includes(path)) {
-	// 			addBadRequestResponseToPathMethods(data);
-	// 			// data = {
-	// 			// 	get: undefined,
-	// 			// 	options: {
-	// 			// 		security: [
-	// 			// 			{
-	// 			// 				cookieAuth: [],
-	// 			// 			}
-	// 			// 		],
-	// 			// 		...data.options
-	// 			// 	},
-	// 			// 	...data
-	// 			// }
-	//
-	// 			console.log(`Route: ${path}, Data: ${JSON.stringify(data, null, 2)}`);
-	// 		}
-	// 	}
-	// }
+		}
+	}
 
 	return docsOAS;
 }
