3423cb0e90e66f08081cbbc76dcdd36b29e8ccf0
al3x-13 <alexandreromba@hotmail.com>
2024-02-03 04:30:27 +0000

    feat: docs generator script

---
+++ backend/docs/generate-oas-json.ts
@@ -0,0 +1,30 @@
+import { generateOpenApi } from "@ts-rest/open-api";
+import { writeFile } from "fs";
+import path from "path";
+import { apiDocsContract } from "@deisi25/types/index";
+import { buildDocsFromTSRestOAS } from "./docs";
+import { OpenAPIObject } from "openapi3-ts/oas31";
+
+const docs = generateOpenApi(apiDocsContract,
+	{
+		info: {
+			title: 'API Docs',
+			description: 'API documentation for AWS Management Portal bakcend API.',
+			version: '1.0.0',
+		},
+	},
+	{
+		jsonQuery: true,
+	}
+);
+
+const docsOAS = buildDocsFromTSRestOAS(docs as OpenAPIObject);
+const docsJson = JSON.stringify(docsOAS, null, 2);
+
+writeFile(path.resolve(__dirname, './oas-schema.json'), docsJson, 'utf8', (err) => {
+	if (err) {
+		console.error('Error while writing API Docs file (JSON): ', err);
+		return;
+	}
+	console.log("API Docs JSON file successfully created ('oas-schema.json')");
+});
