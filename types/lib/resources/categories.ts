11824bcf7081c5b566112ed7136700844fdb47c2
al3x-13 <alexandreromba@hotmail.com>
2024-02-07 06:01:04 +0000

    feat: add base application resource types

---
+++ types/lib/resources/categories.ts
@@ -0,0 +1,9 @@
+enum ResourceCategory {
+	Compute,
+	Containers,
+	Database,
+	Storage
+	// Add new categories here
+};
+
+export default ResourceCategory;
---
+++ types/lib/resources/types.ts
@@ -0,0 +1,10 @@
+export enum ResourceType {
+	EC2 = "Amazon Elastic Compute Cloud (Amazon EC2)",
+	RDS = "Amazon Relational Database Service (Amazon RDS)",
+	S3 = "Amazon Simple Storage Service (Amazon S3)",
+	EBS = "Amazon Elastic Block Store (Amazon EBS)",
+	ECS = "Amazon Elastic Container Service (Amazon ECS)",
+	LAMBDA = "AWS Lambda"
+};
+
+export default ResourceType;
