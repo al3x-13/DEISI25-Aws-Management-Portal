4946086fee853923b62c01f3eba1374e2e696706
TunaPT <asf.antunes@gmail.com>
2024-05-06 18:13:27 +0100

    List users on account management

--- frontend/src/routes/(app)/users/+page.svelte
+++ frontend/src/routes/(app)/users/+page.svelte
@@ -1,72 +1,79 @@
-<script>
-	import { listUsers } from "../../../global/user-instances";
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { listUsers } from "../../../global/user-instances";
 
-	let users = [
-		{ id: 1, username: 'User1', email: 'user1@gmail.com', role: 'Admin', date: '2024-01-12' },
-		{ id: 2, username: 'User2', email: 'user2@gmail.com', role: 'User', date: '2024-02-01' },
-		{ id: 3, username: 'User3', email: 'user3@gmail.com', role: 'User', date: '2024-02-02' }
-	];
+    interface User {
+        id: number;
+        username: string;
+        email: string;
+        role: string;
+        date: string;
+    }
 
-	// function editUser(id) {}
+    let users: User[] = []; 
 
-	// function deleteUser(id) {}
+    onMount(async () => {
+		const userInfoList = await listUsers();
+		users = userInfoList.map(userInfo => ({
+			id: userInfo.id,
+			username: userInfo.username,
+			email: userInfo.email,
+			role: userInfo.role,
+			date: '' 
+		}));
+    });
+
+    // function editUser(id: number) {}
+
+    // function deleteUser(id: number) {}
 </script>
 
 <main>
-	<!-- <button on:click={async () => {console.log(await listUsers());
-	}}>tteeee</button> -->
-	<table>
-		<thead>
-			<tr class="text-color-primary-dark dark:bg-bg2-dark">
-				<th style="text-align: center;">Username</th>
-				<th style="text-align: center;">Email</th>
-				<th style="text-align: center;">Role</th>
-				<th style="text-align: center;">Date</th>
-				<th style="text-align: center;">Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			{#each users as user (user.id)}
-				<tr style="border-bottom: 1px solid white;">
-					<td>{user.username}</td>
-					<td>{user.email}</td>
-					<td>{user.role}</td>
-					<td>{user.date}</td>
-					<td>
-						<!-- <button on:click={() => editUser(user.id)}>
-							<ion-icon name="create-outline" />
-						</button>
-						<button on:click={() => deleteUser(user.id)}>
-							<ion-icon name="trash-outline" />
-						</button> -->
-					</td>
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+    <table>
+        <thead>
+            <tr class="text-color-primary-dark dark:bg-bg2-dark">
+                <th style="text-align: center;">Username</th>
+                <th style="text-align: center;">Email</th>
+                <th style="text-align: center;">Role</th>
+                <th style="text-align: center;">Date</th>
+                <th style="text-align: center;">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each users as user (user.id)}
+                <tr style="border-bottom: 1px solid white;">
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                    <td>{user.date}</td>
+                    <td>
+                        <!-- <button on:click={() => editUser(user.id)}>
+                            <ion-icon name="create-outline" />
+                        </button>
+                        <button on:click={() => deleteUser(user.id)}>
+                            <ion-icon name="trash-outline" />
+                        </button> -->
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </main>
 
 <style>
-	main {
-		display: flex;
-		flex-direction: column;
-		margin: auto;
-	}
+    main {
+        display: flex;
+        flex-direction: column;
+        margin: auto;
+    }
 
-	table {
-		width: 100%;
-		margin-bottom: 20px;
-	}
+    table {
+        width: 100%;
+        margin-bottom: 20px;
+    }
 
-	th,
-	td {
-		padding: 10px;
-		text-align: left;
-	}
-
-	button {
-		padding: 10px;
-		margin-top: 10px;
-		cursor: pointer;
-	}
+    th, td {
+        padding: 10px;
+        text-align: left;
+    }
 </style>
