e5835daae62ec7e66a6ff7f8f950902f41b80efe
TunaPT <asf.antunes@gmail.com>
2024-06-27 09:46:11 +0100

    dashbord page body

--- frontend/src/routes/(app)/root/+page.svelte
+++ frontend/src/routes/(app)/root/+page.svelte
@@ -1,11 +1,43 @@
 <script lang="ts">
+    import PageTitle from '$lib/assets/PageTitle.svelte';
+	import type { PageData } from './$types';
+	import * as Table from '$lib/components/ui/table';
 </script>
 
-<div class="">
-	<div>TESTING</div>
-	<div>TESTING</div>
-	<div>TESTING</div>
-	<div>TESTING</div>
-	<div>TESTING</div>
-	<div>TESTING</div>
+<div class="w-full max-w-5xl flex flex-col items-center jusify-center">
+	<PageTitle
+		title="Users"
+	/>
+
+	<Table.Root class="mt-4 text-base text-text-light dark:text-text-dark">
+		<Table.Header>
+			<Table.Row class="border-border-light dark:border-border-dark">
+				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Username</Table.Head>
+				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Email</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			
+		</Table.Body>
+	</Table.Root>
+
+
+    <div class="mt-20 w-full flex justify-center">
+        <PageTitle
+            title="Pending User Requests"
+        />
+    </div>
+
+    <Table.Root class="mt-4 text-base text-text-light dark:text-text-dark">
+		<Table.Header>
+			<Table.Row class="border-border-light dark:border-border-dark">
+				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Username</Table.Head>
+				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Email</Table.Head>
+                <Table.Head class="text-color-primary-light dark:text-color-primary-dark">Action</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			
+		</Table.Body>
+	</Table.Root>
 </div>
