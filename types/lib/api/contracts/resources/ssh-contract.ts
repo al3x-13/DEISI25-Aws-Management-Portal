13a026cb9c1b6d3bfd2e7abdeb04000524919c6e
al3x-13 <alexandreromba@hotmail.com>
2024-06-27 23:50:00 +0100

    feat(ssh): ssh contract

---
+++ types/lib/api/contracts/resources/ssh-contract.ts
@@ -0,0 +1,103 @@
+import { z } from "zod";
+import { extendZodWithOpenApi } from "@anatine/zod-openapi";
+import { initContract } from "@ts-rest/core";
+import { SSHKeyAccessType, SSHKeyPairType, SSHKeySchema, SSHPrivateKeyFileFormat } from "../../../resources/ssh/ssh";
+import { ApiErrorSchema } from "../../types/error";
+
+extendZodWithOpenApi(z);
+
+const c = initContract();
+
+const sshContract = c.router({
+	create: {
+		method: 'POST',
+		path: '/create',
+		body: z.object({
+			name: z.string().openapi({
+				example: 'webserver-ssh-key'
+			}),
+			keyPairType: z.nativeEnum(SSHKeyPairType).openapi({
+				example: SSHKeyPairType.RSA
+			}),
+			privateKeyFileFormat: z.nativeEnum(SSHPrivateKeyFileFormat).openapi({
+				example: SSHPrivateKeyFileFormat.PEM
+			}),
+			keyAccessType: z.nativeEnum(SSHKeyAccessType).openapi({
+				example: SSHKeyAccessType.Private
+			}),
+		}),
+		responses: {
+			201: z.object({
+				sshKey: SSHKeySchema,
+			}),
+			400: ApiErrorSchema,
+			500: ApiErrorSchema
+		},
+		summary: 'Create SSH key',
+		description: 'Creates a new AWS SSH key. Returns the newly created SSH key if the request succeds.'
+	},
+	delete: {
+		method: 'POST',
+		path: '/delete',
+		body: z.object({
+			keyName: z.string().openapi({
+				example: 'webserver-ssh-key'
+			})
+		}),
+		responses: {
+			200: z.object({
+				success: z.boolean()
+			}),
+			400: ApiErrorSchema,
+			401: ApiErrorSchema,
+			500: ApiErrorSchema
+		},
+		summary: 'Delete SSH key',
+		description: 'Deletes an existing AWS SSH key.'
+	},
+	updateAccessType: {
+		method: 'POST',
+		path: '/updateAccessType',
+		body: z.object({
+			keyName: z.string().openapi({
+				example: 'webserver-ssh-key'
+			}),
+			keyAccessType: z.nativeEnum(SSHKeyAccessType).openapi({
+				example: SSHKeyAccessType.Public
+			})
+		}),
+		responses: {
+			201: z.object({
+				success: z.boolean()
+			}),
+			400: ApiErrorSchema,
+			401: ApiErrorSchema,
+			500: ApiErrorSchema
+		},
+		summary: 'Update access type of SSH key',
+		description: 'Updates the access type of an existing AWS SSH key.'
+	},
+	listKeys: {
+		method: 'GET',
+		path: '/listKeys',
+		query: z.object({
+			maxResults: z.number().optional().openapi({
+				example: 30
+			})
+		}),
+		responses: {
+			200: z.object({
+				sshKeys: z.array(SSHKeySchema),
+			}),
+			400: ApiErrorSchema,
+			401: ApiErrorSchema,
+			500: ApiErrorSchema
+		},
+		summary: 'List all the existing SSH keys.',
+		description: 'Lists all the existing AWS SSH keys available to the requesting user.'
+	}
+},{
+	pathPrefix: '/compute/ssh'
+});
+
+export { sshContract };
