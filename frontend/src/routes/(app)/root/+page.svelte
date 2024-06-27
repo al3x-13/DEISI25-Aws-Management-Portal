e2fa5566d9693f81fd463025081d4296ad1432ca
TunaPT <asf.antunes@gmail.com>
2024-06-27 10:04:36 +0100

    add users list to dashboard

--- frontend/src/routes/(app)/root/+page.svelte
+++ frontend/src/routes/(app)/root/+page.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
     import PageTitle from '$lib/assets/PageTitle.svelte';
-	import type { PageData } from './$types';
 	import * as Table from '$lib/components/ui/table';
+    import { onMount } from 'svelte';
+    // import { listUsers } from "../../../global/user-instances"; // Waiting for merge from user-accounts-management
+
+    interface User {
+        id: number;
+        username: string;
+        email: string;
+        role: string;
+    }
+
+    let users: User[] = []; 
+
+    onMount(async () => {
+		const userInfoList = await listUsers();
+		
+		users = userInfoList.map(userInfo => ({
+			id: userInfo.id,
+			username: userInfo.username,
+			email: userInfo.email,
+			role: userInfo.role,
+		}));
+    });
 </script>
 
 <div class="w-full max-w-5xl flex flex-col items-center jusify-center">
@@ -14,10 +35,17 @@
 			<Table.Row class="border-border-light dark:border-border-dark">
 				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Username</Table.Head>
 				<Table.Head class="text-color-primary-light dark:text-color-primary-dark">Email</Table.Head>
+                <Table.Head class="text-color-primary-light dark:text-color-primary-dark">Role</Table.Head>
 			</Table.Row>
 		</Table.Header>
 		<Table.Body>
-			
+            {#each users as user (user.id)}
+				<Table.Row class="text-lg">
+					<Table.Cell class="text-text-light dark:text-text-dark">{user.username}</Table.Cell>
+					<Table.Cell class="text-text-light dark:text-text-dark">{user.email}</Table.Cell>
+					<Table.Cell class="text-text-light dark:text-text-dark">{user.role}</Table.Cell>
+				</Table.Row>
+			{/each}
 		</Table.Body>
 	</Table.Root>
 
