b795fad7e9c959a1b71c7bc062198de5c5863447
al3x-13 <alexandreromba@hotmail.com>
2024-06-30 23:50:56 +0100

    feat: create user functionality

--- backend/src/lib/users/user-manager.ts
+++ backend/src/lib/users/user-manager.ts
@@ -1,5 +1,7 @@
 import { UserNoHash } from "@deisi25/types/lib/users/users";
 import db from "../../db/db";
+import bcrypt from "bcryptjs";
+
 
 export type Role = {
 	id: number;
@@ -175,6 +177,22 @@ export async function getAllUsers(): Promise<UserNoHash[]> {
 
 
 /**
+ * Get role id with role name.
+ * @param roleName Role name
+ * @returns Role id or null if role name does not exist
+ */
+export async function getRoleIdFromName(roleName: string): Promise<number | null> {
+	const query = await db.query(
+		'SELECT id, name FROM roles WHERE name = $1',
+		[ roleName.toLowerCase() ]
+	);
+
+	if (query.rows.length === 0) return null;
+	return query.rows[0].id;
+}
+
+
+/**
  * Delete a user from the application.
  * @param id User id
  * @returns Whether the user was deleted successfully
@@ -183,6 +201,25 @@ export async function deleteUser(id: number): Promise<boolean> {
 	const query = await db.query(
 		'DELETE FROM users WHERE id = $1',
 		[ id ]
+	);
+
+	return query.rowCount === 1;
+}
+
+/**
+ * Create a new user.
+ * @param username Username
+ * @param email Email
+ * @param password Password
+ * @param role Role
+ * @returns Whether the user creation succeeded
+ */
+export async function createNewUser(username: string, email: string, password: string, role: number): Promise<boolean> {
+	const passwordHash = bcrypt.hashSync(password);
+
+	const query = await db.query(
+		'INSERT INTO users (username, password_hash, email, role) VALUES ($1, $2, $3, $4)',
+		[ username, passwordHash, email, role ]
 	);
 
 	return query.rowCount === 1;
